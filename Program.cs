9c013ff575ed77330fcf47a8cab10faf8aac8eeb
Vladimir <madfrog@e1.ru>
2022-09-13 21:18:57 +0300

    Created Task 8

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿try
+{
+    Console.WriteLine("Введите целое число ");
+    int x=Convert.ToInt32 (Console.ReadLine());
+    int y=x%2;
+    if (y == 0)
+    {
+        Console.WriteLine("является четным числом ");
+    }
+    else Console.WriteLine("не является четным числом ");
+
+}
+catch
+{
+Console.WriteLine("Необходимо ввести целое число цифрами");
+}
